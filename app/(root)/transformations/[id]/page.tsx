e898ea429c052357a78e4566a4705d2423558776
Charalampos Gatsakos <charalamposgatsakos@gmail.com>
2024-04-25 14:55:20 +0300

    SyndaxiAI

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransormationsPage = () => {
+    return (
+        <div>
+            TransormationsPage
+        </div>
+    )
+}
+
+export default TransormationsPage
---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher([
+  '/credits(.*)',
+  '/profile(.*)',
+  '/transformations(.*)',
+]);
+
+ 
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+
+
+export const config = {
+  
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
