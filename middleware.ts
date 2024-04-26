1f191b764238c81c02c1cf18e205be1473ada886
Charalampos Gatsakos <charalamposgatsakos@gmail.com>
2024-04-26 18:51:44 +0300

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/credits(.*)',
-  '/profile(.*)',
-  '/transformations(.*)',
-  '/'
-]);
+const isProtectedRoute = createRouteMatcher( 
+  [
+    '/credits(.*)',
+    '/profile(.*)',
+    '/transformations(.*)',
+    '/'
+  ],
+);
+
 
  
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) auth().protect()
+  
+
 });
+
+
 
 
 
