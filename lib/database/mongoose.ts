cf494d1aeae6df593fb8a1c20cde3386f04635c5
Charalampos Gatsakos <charalamposgatsakos@gmail.com>
2024-04-29 17:57:35 +0300

    db fix 2

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -21,7 +21,7 @@ export const connectToDatabase = async () => {
     if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
     cached.promise = cached.promise || mongoose.connect
-        (MONGODB_URL, { dbName: 'SyntaxiAI', bufferCommands: false })
+        (MONGODB_URL, { dbName: 'sydaxiai', bufferCommands: false })
 
     cached.conn = await cached.promise;
 
