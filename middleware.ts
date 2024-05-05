cc2ee7db6755bbed0d06f56e244b51734242a97c
Charalampos Gatsakos <charalamposgatsakos@gmail.com>
2024-05-05 18:12:59 +0300

    db fix 18

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isProtectedRoute = createRouteMatcher(
   [
     '/credits',
-
+    '/profile',
+    '/transformations',
 
   ],
 );
